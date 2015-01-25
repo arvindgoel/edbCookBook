321f825a4e984ba563d63380a64d53ba6e137261
arvindgoel <arvindgoel@gmail.com>
2015-01-25 00:17:18 -0800

    Update deploytasks.rb

--- deploymentCookBook/recipes/deploytasks.rb
+++ deploymentCookBook/recipes/deploytasks.rb
@@ -1,6 +1,13 @@
 file "/srv/www/edoctorbook/current/protected/runtime" do
   owner 'root'
   group 'root'
-  mode '0755'
+  mode '0777'
+  action :create
+end
+
+file "/srv/www/edoctorbook/current/assets" do
+  owner 'root'
+  group 'root'
+  mode '0777'
   action :create
 end
