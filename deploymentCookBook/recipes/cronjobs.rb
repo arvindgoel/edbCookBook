57d196efb5551676e9b39e2d3dd49126c562c92b
arvindgoel <arvindgoel@gmail.com>
2015-05-25 16:57:35 -0700

    Update cronjobs.rb

--- deploymentCookBook/recipes/cronjobs.rb
+++ deploymentCookBook/recipes/cronjobs.rb
@@ -3,6 +3,11 @@ if node[:opsworks][:instance][:hostname] == 'edb-php-appserver-time-micro'
     hour "6-24"
     minute "10"
     weekday "*"
-    command "cd /srv/www/myapp/current/protected/controllers && php CronController.php CronController actionSmsReminder"
+    command "cd /srv/www/myapp/current/protected && php test.php"
+    Chef::Log.info("Completed Test email send")
+    
+   #command "cd /srv/www/myapp/current/protected/controllers && php CronController.php CronController actionSmsReminder"
+  #  Chef::Log.info("Completed CronController")
+        
   end
 end
